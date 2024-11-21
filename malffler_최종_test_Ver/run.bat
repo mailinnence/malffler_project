a6f2ccf71f3c42835f324eb9ebb026f6a02c6d9c
mailinnence <78959189+mailinnence@users.noreply.github.com>
2024-11-21 20:07:40 +0900

    Update run.bat

--- malffler_최종_test_Ver/run.bat
+++ malffler_최종_test_Ver/run.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM 필요한 패키지 설치
-pip install django proxmoxer pycryptodome markdown openai beautifulsoup4 whitenoise
+pip install django proxmoxer pycryptodome markdown openai beautifulsoup4 whitenoise requests
 
 REM Django 서버 실행
 python manage.py runserver
